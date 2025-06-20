199c24a542872fd7a01963ec37dbeda18415fd51
madebyparry <nicholas@madebyparry.com>
2025-06-19 17:58:51 -0700

    SOme better default styles and logic for individual themes

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,4 +1,13 @@
-# from my custom prompt for work
+# mbp_color_pink EE7777
+# mbp_color_gold EECC11
+# mbp_color_orange C76D14
+# mbp_color_autumn E58844
+# mbp_color_cream F6DF9E
+# mbp_color_red A83832
+# mbp_color_sky 3275A8
+# mbp_color_blue 0C538A
+# mbp_color_teal 14C785
+
 function fish_prompt
     set_color $mbp_color_blue
     set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
@@ -8,9 +17,10 @@ function fish_prompt
     set_color $mbp_color_teal
     echo -n (hostname)' '
     set_color $mbp_color_sky
-    echo -n (prompt_pwd)
+    echo -n '['(prompt_pwd)']'
     set_color $mbp_color_cream
     echo -n ' {'
     echo -n "$git_branch"
     echo -n '} '
+    # fish_prompt_boisonberry
 end
--- functions/fish_prompt_boisonberry.fish
+++ functions/fish_prompt_boisonberry.fish
@@ -1,9 +1,26 @@
+# mbp_color_pink EE7777
+# mbp_color_gold EECC11
+# mbp_color_orange C76D14
+# mbp_color_autumn E58844
+# mbp_color_cream F6DF9E
+# mbp_color_red A83832
+# mbp_color_sky 3275A8
+# mbp_color_blue 0C538A
+# mbp_color_teal 14C785
+
 function fish_prompt_boisonberry
-    set -l textcol  white
-    set -l bgcol    blue
-    set -l arrowcol green
-    set_color $textcol -b $bgcol
-    echo -n " "(basename $PWD)" "
-    set_color $arrowcol -b normal
-    echo -n "⮀ "
+    set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
+
+    set_color $mbp_color_blue
+    echo -n (whoami | cut -c1-2)'⋯ '
+    # set_color $mbp_color_cream
+    # echo -n '@'
+    set_color $mbp_color_gold
+    echo -n "☾✧₊˚⊹ "
+    set_color $mbp_color_teal
+    echo -n '['(prompt_pwd)']'
+    set_color $mbp_color_sky
+    echo -n ' {'
+    echo -n "$git_branch"
+    echo -n '} '
 end
