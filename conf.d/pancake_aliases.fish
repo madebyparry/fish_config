58c3e379381046fb1cac6ecf3f648b4d2fb92d0c
nicholas <nicholas@pancake.madebyparry.online>
2026-03-10 15:24:30 -0700

    Pancake aliases and fixed the og prompt

---
+++ conf.d/pancake_aliases.fish
@@ -0,0 +1,8 @@
+if test $hostname = pancake
+
+    set serv /mnt/wd2tb/serv/
+    set tv /mnt/wd2tb/serv/tv/
+    set mov /mnt/wd2tb/serv/mov/
+    set mus /mnt/wd2tb/serv/mus/
+
+end
--- functions/fish_prompt_og.fish
+++ functions/fish_prompt_og.fish
@@ -1,4 +1,4 @@
-function fish_prompt --description 'Write out the prompt'
+function fish_prompt_og --description 'Write out the prompt'
     set -l last_pipestatus $pipestatus
     set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
     set -l normal (set_color normal)
